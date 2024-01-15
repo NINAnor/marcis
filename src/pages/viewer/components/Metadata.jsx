88de5b70dd7343bacc6dcd5d2881a321e23b1894
Niccolò Cantù <niccolo.cantu@nina.no>
2024-01-15 09:12:16 +0100

    add some style

--- src/pages/viewer/components/Metadata.jsx
+++ src/pages/viewer/components/Metadata.jsx
@@ -1,7 +1,9 @@
+import logoImg from '../../../img/logo_full.png';
+
 export default function Metadata({ title, subtitle, description, metadataRef }) {
   return (
     <div ref={metadataRef} className="metadata">
-      <h3>{title}</h3>
+      <img src={logoImg} className="logo" />
       {subtitle && <p>{subtitle}</p>}
       {description && (<details>
         <summary>Beskrivelse</summary>
